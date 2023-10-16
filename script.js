cc840625f6de9fbaf19632e4c711d715a5fc739e
CHINOGA <43039717+CHINOGA@users.noreply.github.com>
2023-10-16 21:19:31 +0000

    Css and JavaScript added

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Add this code to update the weather container with the respective CSS classes
+
+const weatherContainer = document.getElementById("weather-container");
+weatherContainer.classList.add("weather-container");
+
+const weatherHeader = document.getElementById("weather-header");
+weatherHeader.classList.add("weather-header");
+
+const weatherBody = document.getElementById("weather-body");
+weatherBody.classList.add("weather-body");
